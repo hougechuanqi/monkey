3331cbcd285053cf0772b37a7861ef1ebb408a4c
侯帅鹏 <hou.shuaipeng@immomo.com>
2016-04-11 22:29:49 +0800

    submit URL and collectionutils

---
+++ monkey/monkey-common/src/main/java/org/vrtech/common/URL.java
@@ -0,0 +1,232 @@
+/*
+ * Copyright 2016  VRTECH.
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.vrtech.common;
+
+import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.vrtech.common.utils.CollectionUtils;
+
+/**
+ *
+ * Function description：
+ * 
+ * 
+ * @author houge
+ */
+public class URL implements Serializable {
+
+    private static final long serialVersionUID = 4788299525631405420L;
+
+    private final String protocol;
+    private final String host;
+    private final int port;
+    private final String path;
+    private final Map<String, String> parameters;
+
+    protected URL() {
+        this.protocol = null;
+        this.host = null;
+        this.port = 0;
+        this.path = null;
+        this.parameters = null;
+    }
+
+    public URL(String protocol, String host, int port) {
+        this(protocol, host, port, null, (Map<String, String>) null);
+    }
+
+    public URL(String protocol, String host, int port, String[] pairs) { // 变长参数...与下面的path参数冲突，改为数组
+        this(protocol, host, port, null, CollectionUtils.toStringMap(pairs));
+    }
+
+    public URL(String protocol, String host, int port,
+            Map<String, String> parameters) {
+        this(protocol, host, port, null, parameters);
+    }
+
+    public URL(String protocol, String host, int port, String path) {
+        this(protocol, host, port, path, (Map<String, String>) null);
+    }
+
+    public URL(String protocol, String host, int port, String path,
+            String... pairs) {
+        this(protocol, host, port, path, CollectionUtils.toStringMap(pairs));
+    }
+
+    public URL(String protocol, String host, int port, String path,
+            Map<String, String> parameters) {
+        this.protocol = protocol;
+        this.host = host;
+        this.port = (port < 0 ? 0 : port);
+        this.path = path;
+        // trim the beginning "/"
+        while (path != null && path.startsWith("/")) {
+            path = path.substring(1);
+        }
+        if (parameters == null) {
+            parameters = new HashMap<String, String>();
+        } else {
+            parameters = new HashMap<String, String>(parameters);
+        }
+        this.parameters = Collections.unmodifiableMap(parameters);
+    }
+
+    /**
+     * Parse url string
+     * 
+     * @param url
+     *            URL string
+     * @return URL instance
+     * @see URL
+     */
+    public static URL valueOf(String url) {
+        if (url == null || (url = url.trim()).length() == 0) {
+            throw new IllegalArgumentException("url == null");
+        }
+        String protocol = null;
+        String host = null;
+        int port = 0;
+        String path = null;
+        Map<String, String> parameters = null;
+        int i = url.indexOf("?"); // seperator between body and parameters
+        if (i >= 0) {
+            String[] parts = url.substring(i + 1).split("\\&");
+            parameters = new HashMap<String, String>();
+            for (String part : parts) {
+                part = part.trim();
+                if (part.length() > 0) {
+                    int j = part.indexOf('=');
+                    if (j >= 0) {
+                        parameters.put(part.substring(0, j),
+                                part.substring(j + 1));
+                    } else {
+                        parameters.put(part, part);
+                    }
+                }
+            }
+            url = url.substring(0, i);
+        }
+        i = url.indexOf("://");
+        if (i >= 0) {
+            if (i == 0)
+                throw new IllegalStateException("url missing protocol: \""
+                        + url + "\"");
+            protocol = url.substring(0, i);
+            url = url.substring(i + 3);
+        } else {
+            // case: file:/path/to/file.txt
+            i = url.indexOf(":/");
+            if (i >= 0) {
+                if (i == 0)
+                    throw new IllegalStateException("url missing protocol: \""
+                            + url + "\"");
+                protocol = url.substring(0, i);
+                url = url.substring(i + 1);
+            }
+        }
+
+        i = url.indexOf("/");
+        if (i >= 0) {
+            path = url.substring(i + 1);
+            url = url.substring(0, i);
+        }
+        i = url.indexOf(":");
+        if (i >= 0 && i < url.length() - 1) {
+            port = Integer.parseInt(url.substring(i + 1));
+            url = url.substring(0, i);
+        }
+        if (url.length() > 0)
+            host = url;
+        return new URL(protocol, host, port, path, parameters);
+    }
+
+    public static String encode(String value) {
+        if (value == null || value.length() == 0) {
+            return "";
+        }
+        try {
+            return URLEncoder.encode(value, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    public static String decode(String value) {
+        if (value == null || value.length() == 0) {
+            return "";
+        }
+        try {
+            return URLDecoder.decode(value, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((host == null) ? 0 : host.hashCode());
+        result = prime * result
+                + ((parameters == null) ? 0 : parameters.hashCode());
+        result = prime * result + ((path == null) ? 0 : path.hashCode());
+        result = prime * result + port;
+        result = prime * result
+                + ((protocol == null) ? 0 : protocol.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        URL other = (URL) obj;
+        if (host == null) {
+            if (other.host != null)
+                return false;
+        } else if (!host.equals(other.host))
+            return false;
+        if (parameters == null) {
+            if (other.parameters != null)
+                return false;
+        } else if (!parameters.equals(other.parameters))
+            return false;
+        if (path == null) {
+            if (other.path != null)
+                return false;
+        } else if (!path.equals(other.path))
+            return false;
+        if (port != other.port)
+            return false;
+        if (protocol == null) {
+            if (other.protocol != null)
+                return false;
+        } else if (!protocol.equals(other.protocol))
+            return false;
+        return true;
+    }
+
+}
